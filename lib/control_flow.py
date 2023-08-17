8f3bf097890647f35278d8ab3c86301a50169634
papsel <elijah.njora@student.moringaschool.com>
2023-08-17 20:38:20 +0300

    completed test

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,65 @@
 
 def admin_login(username, password):
     # your code here
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+        
     pass
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature >=40 and temperature <=65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 5 == 0:
+        return "Buzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    else:
+        return num
+
+
+
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        print("Cannot divide by zero!")
+        return None
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    operations_dict = {
+        "+": add,
+        "-": subtract,
+        "*": multiply,
+        "/": divide
+    }
+    
+    if operation in operations_dict:
+        return operations_dict[operation](num1, num2)
+    else:
+        print("Invalid operation!")
+        return None
+
